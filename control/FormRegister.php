a6bb607f4a7a944c21d7ad52928912678b3db453
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-10-27 22:46:57 -0300

    ajustando a class model user

--- control/FormRegister.php
+++ control/FormRegister.php
@@ -1,14 +1,18 @@
 <?php 
 
 require_once 'model/Preferences.php';
-
+require_once 'model/User.php';
 class FormRegister
 {
     private $html;
+    private $data;
 
     public function __construct()
     {
         $this->html = file_get_contents("Layout/html/register.html");
+        $this->data = ['id'       => '',
+                       'email'    => '',
+                       'senha'    => ''];
     }
 
     public function load()
@@ -28,9 +32,25 @@ class FormRegister
         }
     }
 
+    public function save($params)
+    {
+        try 
+        {
+            $pessoa = User::save($params);
+            return  header("Location: index.php?class=Login");
+        } 
+        catch (Exception $e) 
+        {
+            return print $e->getMessage();
+        }  
+    }
+
     public function show()  
     {
         $this->load();
         print $this->html;
     }
 }
+
+
+// TODO - OLHAR A Mensagem de error e para colocar de forma correta no layout
--- model/User.php
+++ model/User.php
@@ -2,28 +2,46 @@
 require_once 'database/TTransaction.php';
 class User
 {
-    public function save ($user)
+    public static function save($user)
     {
         $conn = TTransaction::getConnection();
 
-        if(empty($user['id'])) {
-            $result = $conn->query("SELECT max(id) as next FROM usuarios");
-            $row = $result->fetch();
-            $user['id'] = (int) $row['next'] +1;
-            $sql = "INSERT INTO usuarios (id, login, senha
-                VALUES ( :id, :login, :senha)";
+        if(empty($user['id'])) 
+        {
+            $userExist = $conn->prepare("SELECT * FROM users WHERE email = :email");
+            $userExist->execute([':email' => $user['email']]);
 
+            if($userExist->rowCount() == 0) 
+            {
+                $result = $conn->query("SELECT max(id) as next FROM users");
+                $row = $result->fetch();
+
+                $user['id'] = (int) $row['next'] + 1;
+                $hashedPassword = md5($user['password']);
+
+                $sql = "INSERT INTO users (id, email, password)
+                               VALUES ( :id, :email, :password)";
+            }
+            else 
+            {
+                throw new Exception("Este e-mail já está cadastrado.");
+            }
         }
-        else {
-            $sql = "UPDATE usuarios SET login = :login, senha = :senha WHERE id = :id";
+        else 
+        {
+            $sql = "UPDATE users SET email = :email, password = :password WHERE id = :id";
         }
         $result = $conn->prepare($sql);
-        $result->execute([':id' => $user['id'],':login' => $user['login'],':senha' => $user['senha'] ]);
+        $result->execute([':id' => $user['id'],':email' => $user['email'],':password' => $hashedPassword ]);
+        TTransaction::closeConnection();
     }
+
     public static function all()
     {
         $conn = TTransaction::getConnection();
-        $result = $conn->query("SELECT * FROM usuarios ORDER BY id");
-        return $result->fetchAll();
+        $result = $conn->query("SELECT * FROM users ORDER BY id");
+        $result->fetchAll();
+        TTransaction::closeConnection();
+        return;
     }
 }
